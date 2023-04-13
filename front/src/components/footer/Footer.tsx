908eefae92ebf9da089ba25860eca0117958887e
Roberto Pontes <robertopnts13@gmail.com>
2023-04-13 14:34:37 -0300

    feat: component footer concluded, including the scroll to top function

---
+++ front/src/components/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { ChevronUpIcon } from "@chakra-ui/icons"
+import { Box, Button, IconButton, Text } from "@chakra-ui/react"
+
+const Footer = () => {
+
+    const scrollToTop = () => {
+        window.scrollTo({top:0, behavior: 'smooth'})
+    }
+
+    return (
+        <Box display={'flex'} flexDir={{base:'column', md: 'row'}} bg='grey.900' color={'white'} h={{base:'310px', md: '140px'}} w={'100vw'} justifyContent={'space-around'} alignItems={'center'}>
+            <Text fontWeight={"bold"} fontSize={24}> 
+                        Motors <Text as={'span'} fontSize={16}>shop</Text>
+                    </Text>
+            <Text>© 2023 - Todos os direitos reservados.</Text>
+            <IconButton icon={<ChevronUpIcon/>} aria-label={"Voltar ao Topo"} bg={'grey.800'} onClick={scrollToTop}/>
+        </Box>
+    )
+}
+
+export default Footer
