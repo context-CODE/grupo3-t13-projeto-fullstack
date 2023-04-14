aec9f954f7b88693fd78dc3a2f86031793083c11
Roberto Pontes <robertopnts13@gmail.com>
2023-04-14 16:48:17 -0300

    fix: footer corrigido

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     }
 
     return (
-        <Box display={'flex'} flexDir={{base:'column', md: 'row'}} bg='grey.900' color={'white'} h={{base:'310px', md: '140px'}} w={'100vw'} justifyContent={'space-around'} alignItems={'center'}>
+        <Box display={'flex'} flexDir={{base:'column', md: 'row'}} pr={{base: 'none', md:'60px'}} pl={{base: 'none', md:'60px'}} bg='grey.900' color={'white'} pt={{base: '45px', md: 'none'}} pb={{base: '45px', md: 'none'}} h={{base:'310px', md: '140px'}} w={'100vw'} justifyContent={'space-between'} alignItems={'center'}>
             <Image src='/assets/Motors-shop-footer.svg' alt="footer img"/>
             <Text variant={'body-2-400'}>© 2023 - Todos os direitos reservados.</Text>
             <IconButton icon={<ChevronUpIcon/>} aria-label={"Voltar ao Topo"} bg={'grey.800'} onClick={scrollToTop}/>
