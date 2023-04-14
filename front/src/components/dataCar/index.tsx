c7e668ceb71b0d6fe2488604df5e721a82fa3f2b
Angelica Assini <assini@gmail.com>
2023-04-14 10:05:25 -0300

    added typography

--- front/src/components/dataCar/index.tsx
+++ front/src/components/dataCar/index.tsx
@@ -1,34 +1,32 @@
-import { Flex, Box, Button,  Card, CardHeader, CardBody, Stack, Heading, Tag, Divider, CardFooter, 
+import { Flex, Button,  Card, CardBody, Stack, Heading, Tag, CardFooter, 
     Image, Text   } from '@chakra-ui/react'
-
-
 
 export const DataCar = () => {
   return (
-        <Flex flexDir={'column'} gap='1' >
+        <Flex maxW={{ base: '90%', sm: '440px' }} flexDir={'column'} gap='1' >
             <Image
             src="/assets/CarDetailHome.png"
             alt='Car photo'
             borderRadius='lg'
             />
-            <Card justifyContent={'center'} alignItems={'center'}>
+            <Card justifyContent={'center'} >
                 <CardBody  w='100%' >
-                    <Stack p='16px' mt='6' spacing='3'>
-                        <Heading textAlign={'center'} size='md' color={'gray.800'}>Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200</Heading>         
-                        <Flex justifyContent={'space-between'}>
+                    <Stack p={'16px'} mt={'6'} spacing={'3'}>
+                        <Heading variant={'Heading-6-600'} textAlign={'center'} size='md'>Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200</Heading>         
+                        <Flex justifyContent={'space-between'} flexDirection={{ base:'column', sm:'row'}} gap={'10'}>
                             <Flex flexDir="row" gap='2' alignItems={'center'}>
                                 <Tag>2013</Tag>  
                                 <Tag>0 KM</Tag>
                             </Flex>
-                            <Text color={'gray.800'} fontSize='2xl'>
+                            <Text color={'gray.800'} variant={{ base:'Heading-6-600', sm:'Heading-7-500'}}>
                                 R$ 00.000,00
                             </Text>
                         </Flex>
                     </Stack>
                 </CardBody>
         
-                <CardFooter>
-                    <Button variant='default'>
+                <CardFooter >
+                    <Button justifyContent={'left'} variant={'default'} height={{ base: '38px', sm: '48px' }}>
                         Comprar                 
                     </Button>
                 </CardFooter>
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,10 @@
-import { DataCar} from '@/components/dataCar';
+import { DataCar } from '@/components/dataCar';
 import { Box, Heading } from '@chakra-ui/react';
 export default function Home() {
   return (
     <Box p="16px">
       <Heading>Home</Heading>
-    
-      <DataCar/>
+
     </Box>
   );
 }
