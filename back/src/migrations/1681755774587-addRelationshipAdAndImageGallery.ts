27aa8f799abc6bc7d7448f3cae59561befda44ac
CarlosX26 <cjunior1@live.com>
2023-04-17 15:23:56 -0300

    fix: migration relationship between ad and image gallery

--- back/src/migrations/1681755774587-addRelationshipAdAndImageGallery.ts
+++ back/src/migrations/1681755774587-addRelationshipAdAndImageGallery.ts
@@ -1,16 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddRelationshipAdAndImagesGallery1681755087546 implements MigrationInterface {
-    name = 'AddRelationshipAdAndImagesGallery1681755087546'
+export class AddRelationshipAdAndImageGallery1681755774587 implements MigrationInterface {
+    name = 'AddRelationshipAdAndImageGallery1681755774587'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "image_gallery" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "image_url" character varying(150) NOT NULL, "advertisementId" uuid, CONSTRAINT "PK_278b7928303c6293cf98ed65172" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "image_gallery" ADD "advertisementId" uuid`);
         await queryRunner.query(`ALTER TABLE "image_gallery" ADD CONSTRAINT "FK_f2ddd86a30b1fdc78a0a238faf2" FOREIGN KEY ("advertisementId") REFERENCES "advertisements"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "image_gallery" DROP CONSTRAINT "FK_f2ddd86a30b1fdc78a0a238faf2"`);
-        await queryRunner.query(`DROP TABLE "image_gallery"`);
+        await queryRunner.query(`ALTER TABLE "image_gallery" DROP COLUMN "advertisementId"`);
     }
 
 }
