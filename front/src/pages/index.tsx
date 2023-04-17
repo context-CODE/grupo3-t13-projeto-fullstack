59519f8dd729e29a5787f6a024d685a0ee02a789
Roberto Pontes <robertopnts13@gmail.com>
2023-04-17 13:55:24 -0300

    commit for pull

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,10 +2,12 @@ import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
 import ProductCard from '@/components/productCard/ProductCard';
 import { Box, Heading } from '@chakra-ui/react';
+
+
 export default function Home() {
   return (
-    <Box p="16px">
-      <Heading>Home</Heading>
-    </Box>
+    <>
+    Home
+    </>
   );
 }
