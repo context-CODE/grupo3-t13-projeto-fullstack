839d6c2922d323fe4ad4b1d9f907010fc90e5f48
CarlosX26 <cjunior1@live.com>
2023-04-17 12:46:58 -0300

    feat: add migration start address

---
+++ back/src/migrations/1681746344368-startAddressEntity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class StartAddressEntity1681746344368 implements MigrationInterface {
+    name = 'StartAddressEntity1681746344368'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "adresses" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "zipCode" character varying(8) NOT NULL, "state" character varying(2) NOT NULL, "city" character varying(25) NOT NULL, "street" character varying(40) NOT NULL, "number" integer NOT NULL, "complement" character varying(128), CONSTRAINT "PK_2787c84f7433e390ff8961d552d" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "adresses"`);
+    }
+
+}
