510df05ec5b691d3f9993db6402895e027c4e346
SamSKS <samuel.meireles.15s@outlook.com>
2023-04-19 00:16:06 -0300

    fix: home for base

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,20 +1,9 @@
-// import { FilterAdvertisements } from '@/components/FilterAd';
-import CardList from '@/components/homepage/CardList';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <Box display={'flex'} justifyContent={'center'} w={'100vw'} bg={'white'}>
-      <Box
-        w={'1200px'}
-        minW={'375px'}
-        maxW={'1032px'}
-        display={'flex'}
-        justifyContent={{ base: 'center', md: 'space-between' }}
-      >
-        {/* <FilterAdvertisements /> */}
-
-        <CardList />
-      </Box>
+    <Box>
+      <Heading>Home</Heading>
     </Box>
   );
 }
