43197a05cbfd019230622196d20e823def26a1a8
Carlos Junio <106787456+CarlosX26@users.noreply.github.com>
2023-04-24 17:02:31 -0300

    Update user.context.ts

--- front/src/types/user.context.ts
+++ front/src/types/user.context.ts
@@ -1,11 +1,1 @@
-import { z } from 'zod';
-import { usersReqSchema, usersResSchema } from '../schemas/users.schema';
 
-import User from '../../../back/src/entities/user.entity';
-import { DeepPartial, Repository } from 'typeorm';
-
-export type iUserEntity = Repository<User>;
-
-export type iUserReq = z.infer<typeof usersReqSchema>;
-export type iUserRes = z.infer<typeof usersResSchema>;
-export type iUserReqUpdate = DeepPartial<iUserReq>;
