2b3ba2fc15b6bdd8cefeb755908df28791872f28
CarlosX26 <cjunior1@live.com>
2023-04-26 16:08:03 -0300

    feat: add route reset password

--- back/src/routes/users.routes.ts
+++ back/src/routes/users.routes.ts
@@ -3,10 +3,23 @@ import createUserController from "../controllers/users/createUser.controller";
 import retrieveUserController from "../controllers/users/retrieveUser.controller";
 import ensureAuthMiddleware from "../middlewares/authentication/ensureAuth.middleware";
 import ensureIsAdvertiser from "../middlewares/authorization/ensureIsAdvertiser.middleware";
+import resetPasswordSendMailController from "../controllers/users/resetPasswordSendMail.controller";
+import ensureIsValidDataMiddleware from "../middlewares/formHandling/ensureIsValidData.middleware";
+import { userReqResetPasswordSchema } from "../schemas/users.schema";
 
 const usersRoutes = Router();
 
-usersRoutes.post("", createUserController)
-usersRoutes.get("/profile", ensureAuthMiddleware, ensureIsAdvertiser, retrieveUserController)
+usersRoutes.post("", createUserController);
+usersRoutes.post(
+  "/resetPassword",
+  ensureIsValidDataMiddleware(userReqResetPasswordSchema),
+  resetPasswordSendMailController
+);
+usersRoutes.get(
+  "/profile",
+  ensureAuthMiddleware,
+  ensureIsAdvertiser,
+  retrieveUserController
+);
 
 export default usersRoutes;
