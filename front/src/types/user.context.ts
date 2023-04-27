f1fc067b07e72cf4f0f0b63419ad912b205b2418
Angelica Assini <assini@gmail.com>
2023-04-27 20:38:12 -0300

    schemas and interfaces created or modified

--- front/src/types/user.context.ts
+++ front/src/types/user.context.ts
@@ -1,1 +1,23 @@
+import { z } from 'zod';
+import { usersReqSchema } from '../schemas/users.schema';
+import { DeepPartial } from '@chakra-ui/react';
+import { iAddressReq } from './address.context';
 
+export type iUserReq = z.infer<typeof usersReqSchema>;
+
+export type iUserReqUpdate = DeepPartial<iUserReq>;
+
+export interface iUserRes {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone_number: string;
+  birthdate: string;
+  profile_img: string;
+  is_advertiser: boolean;
+  description?: string;
+  created_at: Date;
+  updated_at: Date;
+  address: iAddressReq;
+}
