ecbca4514e4768d68a964ab889c118db36c47baf
Carlos Junio <cjunior1@live.com>
2023-05-11 12:12:12 -0300

    fix: page ad

--- front/src/pages/advertisements/[advertisementId].tsx
+++ front/src/pages/advertisements/[advertisementId].tsx
@@ -48,6 +48,7 @@ export default function Advertisement({ advertisement }: iAdProps) {
 
   useEffect(() => {
     setCurrentAdvertisement(advertisement);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
